affcb1e7f5f53d5cf0c03928f4d5c44da4e7000b
Nuno Ribeiro <nmpribeiro@gmail.com>
2021-11-23 11:30:14 +0100

    Tests fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // import logo from "./logo.svg";
 import { AppNavbar } from "./components";
@@ -7,12 +7,12 @@ import { About, Home, Search } from "./pages";
 import "./App.css";
 
 export const App: React.FC = () => (
-  <div>
+  <BrowserRouter>
     <AppNavbar />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/search" element={<Search />} />
     </Routes>
-  </div>
+  </BrowserRouter>
 );
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 import { App } from "./App";
@@ -8,9 +7,7 @@ import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
