1a239356014fa71b52f4d1925c8ce53c16217727
Nuno Ribeiro <nmpribeiro@gmail.com>
2021-11-25 14:02:53 +0100

    Decouples buttons from main Drawer component

--- src/components/drawers/TechDrawer.tsx
+++ src/components/drawers/TechDrawer.tsx
@@ -1,62 +1,26 @@
 import React from "react";
-
+import { Button, Heading } from "@chakra-ui/react";
 import { LoremIpsum } from "react-lorem-ipsum";
-import {
-  Box,
-  Button,
-  Collapse,
-  useDisclosure,
-  Heading,
-} from "@chakra-ui/react";
+import { FaServer } from "react-icons/fa";
 
-interface Props {
-  height?: number;
-  width?: number;
-}
+import { Drawer } from "./components/Drawer";
 
-export const TechDrawer: React.FC<Props> = ({ height = 400, width = 350 }) => {
-  const { isOpen, onToggle } = useDisclosure();
-
-  return (
-    <div
-      style={{
-        position: "relative",
-        width: isOpen ? width : 42,
-        height: isOpen ? height : 70,
-        transition: "all 0.5s",
-        marginBottom: 10,
-      }}
-    >
-      <Collapse in={isOpen} animateOpacity>
-        <Box color="white" rounded="md" shadow="md" maxW={width} maxH={height}>
-          <div style={{ padding: 20, paddingLeft: 50, height }}>
-            <div
-              style={{
-                height: "100%",
-                overflow: "auto",
-                color: "black",
-                paddingRight: 5,
-              }}
-            >
-              <Heading as="h6" fontSize="20">
-                Technologies
-              </Heading>
-              <LoremIpsum p={2} />
-            </div>
-          </div>
-        </Box>
-      </Collapse>
-
-      <div
-        style={{
-          transform: "rotate(-90deg)",
-          transformOrigin: "top left",
-          position: "absolute",
-          top: 68,
-        }}
+export const TechDrawer: React.FC = () => (
+  <Drawer
+    label="Tech"
+    icon={({ onToggle }) => (
+      <Button
+        onClick={onToggle}
+        borderRadius={30}
+        style={{ height: 40, width: 40 }}
       >
-        <Button onClick={onToggle}>Tech</Button>
-      </div>
-    </div>
-  );
-};
+        <FaServer style={{ marginRight: 5, transform: "rotate(90deg)" }} />
+      </Button>
+    )}
+  >
+    <Heading as="h6" fontSize="20">
+      Technologies
+    </Heading>
+    <LoremIpsum p={2} />
+  </Drawer>
+);
---
+++ src/components/drawers/components/Drawer.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+
+import { Box, Collapse, useDisclosure } from "@chakra-ui/react";
+
+interface Props {
+  label: string;
+  height?: number;
+  width?: number;
+  icon: React.FC<{ onToggle: () => void }>;
+}
+
+export const Drawer: React.FC<Props> = ({
+  children,
+  label = "Rename me please",
+  height = 400,
+  width = 350,
+  icon: Icon,
+}) => {
+  const { isOpen, onToggle } = useDisclosure();
+
+  return (
+    <div
+      style={{
+        position: "relative",
+        width: isOpen ? width : 42,
+        height: isOpen ? height : 70,
+        transition: "all 0.5s",
+        marginBottom: 10,
+      }}
+    >
+      <Collapse in={isOpen} animateOpacity>
+        <Box color="white" rounded="md" shadow="md" maxW={width} maxH={height}>
+          <div style={{ padding: 20, paddingLeft: 50, height }}>
+            <div
+              style={{
+                height: "100%",
+                overflow: "auto",
+                color: "black",
+                paddingRight: 5,
+              }}
+            >
+              {children}
+            </div>
+          </div>
+        </Box>
+      </Collapse>
+
+      <div
+        style={{
+          transform: "rotate(-90deg)",
+          transformOrigin: "top left",
+          position: "absolute",
+          top: 68,
+        }}
+      >
+        {<Icon onToggle={onToggle} />}
+      </div>
+    </div>
+  );
+};
