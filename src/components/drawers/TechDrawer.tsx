df1502837acb6fee119f78a12a757fafadab6f96
Nuno Ribeiro <nmpribeiro@gmail.com>
2021-11-25 10:51:01 +0100

    Tries to add the cool UI tech list and filter as discussed

--- src/components/drawers/TechDrawer.tsx
+++ src/components/drawers/TechDrawer.tsx
@@ -7,26 +7,32 @@ export const TechDrawer: React.FC = () => {
   const { isOpen, onToggle } = useDisclosure();
 
   return (
-    <div style={{ position: 'relative', padding: 10 }}>
-      <div style={{ height: 60, width: 20 }} />
+    <div style={{ position: "relative", padding: 10, minHeight: 70 }}>
+      <div style={{ marginTop: -25 }}>
+        <Collapse in={isOpen} animateOpacity>
+          <Box
+            p="40px"
+            color="white"
+            mt="4"
+            bg="teal.500"
+            rounded="md"
+            shadow="md"
+            maxW={300}
+          >
+            <div>Technologies</div>
+            <LoremIpsum p={1} />
+          </Box>
+        </Collapse>
+      </div>
+
       <div
-        style={{ rotate: "-90deg", left: -5, top: 20, position: "absolute" }}
+        style={{ rotate: "-90deg", left: -5, top: 15, position: "absolute" }}
       >
         <Button onClick={onToggle}>Tech</Button>
       </div>
-      <Collapse in={isOpen} animateOpacity>
-        <Box
-          p="40px"
-          color="white"
-          mt="4"
-          bg="teal.500"
-          rounded="md"
-          shadow="md"
-          maxW={300}
-        >
-          <LoremIpsum p={1} />
-        </Box>
-      </Collapse>
+      {/* <Collapse in={!isOpen} animateOpacity>
+        <div style={{ height: 60, width: 20 }} />
+      </Collapse> */}
     </div>
   );
 };
