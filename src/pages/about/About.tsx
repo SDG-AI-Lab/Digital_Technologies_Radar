10c63bf4372b2edea7f8626a07b1f4c8637a3505
OzgeOzkaya <ozgeozzkaya@gmail.com>
2022-04-27 10:27:45 +0300

    feedback url

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -2,10 +2,18 @@ import React from 'react';
 import { Container, VStack } from '@chakra-ui/react';
 import AboutOrganization from './AboutOrganization';
 import { aboutContentList } from './AboutContent';
+import { Text, Badge, Button } from '@chakra-ui/react';
 
 export const About: React.VFC = () => {
-  return (
+    return (
     <Container my={7} centerContent maxW='container.xl' overflowY={'auto'}>
+      <Button
+          position={'absolute'}       
+          colorScheme='orange'
+          right={30}  
+        >
+          Send us your feedback
+      </Button>
       <VStack>
         {aboutContentList.map((aboutContentChild, index) => (
           <div className='about-content' key={index}>
