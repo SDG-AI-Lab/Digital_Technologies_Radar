fb766eb09a3e5f85ad73c11f93b1a3f25daff943
thomibackup <zekariabi@gmail.com>
2022-04-27 10:11:24 +0300

    add image to the popup modal and merge disaster cycles of similar ideas/concepts/examples

--- src/pages/search/SearchBar.tsx
+++ src/pages/search/SearchBar.tsx
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Center, Input, Stack, Text } from '@chakra-ui/react';
 
 import SearchResult from './SearchResult';
 
-import { BaseCSVType, useRadarState } from '@undp_sdg_ai_lab/undp-radar';
+import { BaseCSVType, useRadarState} from '@undp_sdg_ai_lab/undp-radar';
 
 export const SearchBar: React.FC = () => {
+  const [mergedTechs, setMergedTechs] = useState<BaseCSVType[]>([]);
+
   const [newFilter, setNewFilter] = useState<BaseCSVType[]>([]);
   const [filteredTech, setFilteredTech] = useState<BaseCSVType[]>([]);
   const [techSearch, setTechSearch] = useState('');
@@ -13,25 +15,64 @@ export const SearchBar: React.FC = () => {
     state: { blips }
   } = useRadarState();
 
+    useEffect(() => {
+       
+        mergeDiasterCycle();
+
+    }, [blips]);
+
+
+    let merge: BaseCSVType[] = [];
+
+
+    /* Merge DisasterCycle of Techs with similar Ideas/Concepts/Examples */
+    const mergeDiasterCycle = () => {
+
+        blips.forEach(function(item) {
+          var existingBips = merge.filter(function(v, i) {
+            return v["Ideas/Concepts/Examples"] == item["Ideas/Concepts/Examples"];
+          });
+          
+          
+          if (existingBips.length) {
+            var existingIndex = merge.indexOf(existingBips[0]);
+            merge[existingIndex]['Disaster Cycle'] = merge[existingIndex]['Disaster Cycle'].concat(item['Disaster Cycle']);
+
+          } else {
+            if (typeof item['Disaster Cycle'] == 'string')
+              item['Disaster Cycle'] = [item['Disaster Cycle']];
+            merge.push(item);
+          }
+          
+        });
+
+        setMergedTechs(merge);
+    }
+
+
+  /* Handle input change events, filter(search) based on this change events */
+
   const handleFilter = (event: React.ChangeEvent<HTMLInputElement>) => {
+
     const searchword = event.target.value.toLowerCase();
     setTechSearch(searchword);
     const query = searchword.toLowerCase();
 
-    console.log('blips ', blips);
-    const newFilter: BaseCSVType[] = blips.filter((value) => {
+    const newFilter: BaseCSVType[] = mergedTechs.filter((value) => {
       return (
         value['Ideas/Concepts/Examples'].toLowerCase().includes(query) ||
         value.Description.toLowerCase().includes(query) ||
         value['Use Case'].toLowerCase().includes(query) ||
-        value['Disaster Cycle'].toLowerCase().includes(query) ||
-        value['Un Host Organisation'].toLowerCase().includes(query) ||
+        value['Disaster Cycle'].toString().toLowerCase().includes(query) ||
+        value['Un Host Organisation'].toString().toLowerCase().includes(query) ||
         value['Country of Implementation'].toLowerCase().includes(query) ||
         value['SDG'].toString().toLowerCase().includes(query)
       );
     });
 
+
     setNewFilter(newFilter);
+
 
     if (query === '') {
       setFilteredTech([]);
@@ -39,6 +80,9 @@ export const SearchBar: React.FC = () => {
       setFilteredTech(newFilter);
     }
   };
+
+
+
 
   return (
     <div>
@@ -59,7 +103,7 @@ export const SearchBar: React.FC = () => {
             fontSize={'sm'}
             letterSpacing={1.1}
           >
-            Found {newFilter.length} out of {blips.length}
+           
           </Text>
         </Stack>
       </Center>
@@ -68,7 +112,7 @@ export const SearchBar: React.FC = () => {
         <SearchResult filteredContent={filteredTech} />
       )}
 
-      {filteredTech.length === 0 && <SearchResult filteredContent={blips} />}
+      {filteredTech.length === 0 && <SearchResult filteredContent={mergedTechs} />}
     </div>
   );
 };
