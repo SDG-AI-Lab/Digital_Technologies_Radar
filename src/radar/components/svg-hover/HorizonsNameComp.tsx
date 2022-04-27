0f71cdbf03d83a6aca5c4a4d0a6f3958fc88588e
Eyoab Tesfaye <eyoab@dulcons.com>
2022-04-27 12:53:33 +0300

    Removed unsed vars

--- src/radar/components/svg-hover/HorizonsNameComp.tsx
+++ src/radar/components/svg-hover/HorizonsNameComp.tsx
@@ -3,15 +3,6 @@ import { ReactLabelComp } from '@undp_sdg_ai_lab/undp-radar';
 
 const style = { cursor: 'pointer' };
 
-const HEIGHT = 3;
-const WIDTH = 20;
-const SPACE = 2;
-const O = -15;
-const O1 = O + 1 * HEIGHT + 1 * SPACE;
-const O2 = O + 2 * HEIGHT + 2 * SPACE;
-const O3 = O + 3 * HEIGHT + 3 * SPACE;
-const O4 = O + 4 * HEIGHT + 4 * SPACE;
-
 export const HorizonsNameComp: ReactLabelComp = ({
   onMouseEnter,
   onMouseMove,
