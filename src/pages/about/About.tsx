f9efed6bc5337f62fdde5976da978091e20cb9da
OzgeOzkaya <ozgeozzkaya@gmail.com>
2022-04-27 17:39:46 +0300

    feedback button added

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
-import { Container, VStack } from '@chakra-ui/react';
+import { Container, VStack, Button } from '@chakra-ui/react';
 import AboutOrganization from './AboutOrganization';
 import { aboutContentList } from './AboutContent';
 
 export const About: React.VFC = () => {
+  const onFeedbackClick = () => window.open('https://forms.office.com/pages/responsepage.aspx?id=Xtvls0QpN0iZ9XSIrOVDGUdlGNAMNehOnzuv5w8XXdVUMVVIVUlVWFVQWEU1TVRHODBURVc2SE1YOC4u','_newtab');
   return (
     <Container my={7} centerContent maxW='container.xl' overflowY={'auto'}>
+      <Button
+          onClick={onFeedbackClick}
+          style={{cursor: 'pointer'}}
+          position={'absolute'}       
+          colorScheme='orange'
+          right={30}  
+        >
+          Send us your feedback
+      </Button>
       <VStack>
         {aboutContentList.map((aboutContentChild, index) => (
           <div className='about-content' key={index}>
