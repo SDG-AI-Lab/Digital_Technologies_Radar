ca63c30b1718035cf15aedd537deec5c0815a487
OzgeOzkaya <ozgeozzkaya@gmail.com>
2022-04-27 17:22:56 +0300

    clickable feedback survey button

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -5,9 +5,12 @@ import { aboutContentList } from './AboutContent';
 import { Text, Badge, Button } from '@chakra-ui/react';
 
 export const About: React.VFC = () => {
+  const onFeedbackClick = () => window.open('https://forms.office.com/pages/responsepage.aspx?id=Xtvls0QpN0iZ9XSIrOVDGUdlGNAMNehOnzuv5w8XXdVUMVVIVUlVWFVQWEU1TVRHODBURVc2SE1YOC4u','_newtab');
     return (
     <Container my={7} centerContent maxW='container.xl' overflowY={'auto'}>
       <Button
+          onClick={onFeedbackClick}
+          style={{cursor: 'pointer'}}
           position={'absolute'}       
           colorScheme='orange'
           right={30}  
