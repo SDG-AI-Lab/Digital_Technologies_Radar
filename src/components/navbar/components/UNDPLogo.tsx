33fc20c0b5e7ca3faae8ae284cb415610c3b1d1a
Nuno Ribeiro <nmpribeiro@gmail.com>
2022-04-28 08:59:04 +0200

    Movs onclick to Logo component so it becomes more generic

--- src/components/navbar/components/UNDPLogo.tsx
+++ src/components/navbar/components/UNDPLogo.tsx
@@ -9,15 +9,13 @@ import logoWhite from '../../../assets/logos/SDG_WHITE_logo.png';
 
 export const UNDPLogo: React.FC<ChakraProps> = (props) => {
   const { colorMode } = useColorMode();
-  const onLogoClick = () => window.open('https://sdgailab.org','_newtab');
   return (
-    <button onClick={onLogoClick} style={{cursor: 'pointer'}}>
-      <Logo
-        w='65px'
-        file={colorMode === 'light' ? logoBlack : logoWhite}
-        maxwidthorheight={65}
-        {...props}
-      />
-    </button>
+    <Logo
+      w='65px'
+      file={colorMode === 'light' ? logoBlack : logoWhite}
+      maxwidthorheight={65}
+      onClick={() => window.open('https://sdgailab.org', '_newtab')}
+      {...props}
+    />
   );
 };
