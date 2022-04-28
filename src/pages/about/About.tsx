df8be5126bf1099cdfbee2779c2d99266d81d1c2
Nuno Ribeiro <nmpribeiro@gmail.com>
2022-04-28 08:48:30 +0200

    Slight fix on components on top of each other

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -4,25 +4,38 @@ import AboutOrganization from './AboutOrganization';
 import { aboutContentList } from './AboutContent';
 
 export const About: React.VFC = () => {
-  const onFeedbackClick = () => window.open('https://forms.office.com/pages/responsepage.aspx?id=Xtvls0QpN0iZ9XSIrOVDGUdlGNAMNehOnzuv5w8XXdVUMVVIVUlVWFVQWEU1TVRHODBURVc2SE1YOC4u','_newtab');
+  const onFeedbackClick = () =>
+    window.open(
+      'https://forms.office.com/pages/responsepage.aspx?id=Xtvls0QpN0iZ9XSIrOVDGUdlGNAMNehOnzuv5w8XXdVUMVVIVUlVWFVQWEU1TVRHODBURVc2SE1YOC4u',
+      '_newtab'
+    );
   return (
-    <Container my={7} centerContent maxW='container.xl' overflowY={'auto'}>
+    <>
       <Button
-          onClick={onFeedbackClick}
-          style={{cursor: 'pointer'}}
-          position={'absolute'}       
-          colorScheme='orange'
-          right={30}  
-        >
-          Send us your feedback
+        onClick={onFeedbackClick}
+        style={{ cursor: 'pointer' }}
+        position={'absolute'}
+        colorScheme='orange'
+        right={30}
+        top={5}
+      >
+        Send us your feedback
       </Button>
-      <VStack>
-        {aboutContentList.map((aboutContentChild, index) => (
-          <div className='about-content' key={index}>
-            <AboutOrganization organizationContent={aboutContentChild} />
-          </div>
-        ))}
-      </VStack>
-    </Container>
+      <Container
+        mt={20}
+        mb={7}
+        centerContent
+        maxW='container.xl'
+        overflowY={'auto'}
+      >
+        <VStack>
+          {aboutContentList.map((aboutContentChild, index) => (
+            <div className='about-content' key={index}>
+              <AboutOrganization organizationContent={aboutContentChild} />
+            </div>
+          ))}
+        </VStack>
+      </Container>
+    </>
   );
 };
