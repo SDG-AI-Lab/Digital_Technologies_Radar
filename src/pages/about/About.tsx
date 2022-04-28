50bdfe0e4b192854942f35fb9ecab3e85fe36cf2
Özge Özkaya <ozgeozzkaya@gmail.com>
2022-04-28 09:54:04 +0300

    feedback button added (#138)
    * feedback button added
    
    * Slight fix on components on top of each other
    
    Co-authored-by: Nuno Ribeiro <nmpribeiro@gmail.com>

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,18 +1,41 @@
 import React from 'react';
-import { Container, VStack } from '@chakra-ui/react';
+import { Container, VStack, Button } from '@chakra-ui/react';
 import AboutOrganization from './AboutOrganization';
 import { aboutContentList } from './AboutContent';
 
 export const About: React.VFC = () => {
+  const onFeedbackClick = () =>
+    window.open(
+      'https://forms.office.com/pages/responsepage.aspx?id=Xtvls0QpN0iZ9XSIrOVDGUdlGNAMNehOnzuv5w8XXdVUMVVIVUlVWFVQWEU1TVRHODBURVc2SE1YOC4u',
+      '_newtab'
+    );
   return (
-    <Container my={7} centerContent maxW='container.xl' overflowY={'auto'}>
-      <VStack>
-        {aboutContentList.map((aboutContentChild, index) => (
-          <div className='about-content' key={index}>
-            <AboutOrganization organizationContent={aboutContentChild} />
-          </div>
-        ))}
-      </VStack>
-    </Container>
+    <>
+      <Button
+        onClick={onFeedbackClick}
+        style={{ cursor: 'pointer' }}
+        position={'absolute'}
+        colorScheme='orange'
+        right={30}
+        top={5}
+      >
+        Send us your feedback
+      </Button>
+      <Container
+        mt={20}
+        mb={7}
+        centerContent
+        maxW='container.xl'
+        overflowY={'auto'}
+      >
+        <VStack>
+          {aboutContentList.map((aboutContentChild, index) => (
+            <div className='about-content' key={index}>
+              <AboutOrganization organizationContent={aboutContentChild} />
+            </div>
+          ))}
+        </VStack>
+      </Container>
+    </>
   );
 };
