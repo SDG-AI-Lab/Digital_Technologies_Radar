1616a2ec0be7116079cad7e4b6900fc24869fab2
Jackson <jackson.onyango@undp.org>
2023-01-13 09:40:56 +0300

    Do not center home page

--- src/ui/MainLayout.tsx
+++ src/ui/MainLayout.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import cx from 'classnames';
 import { Flex } from '@chakra-ui/react';
+import './MainLayout.scss';
+import { useLocation } from 'react-router-dom';
 
-export const MainLayout: React.FC = ({ children }) => (
-  <Flex flex={1} direction={'column'} maxW={1800} margin={'0 auto'}>
-    {children}
-  </Flex>
-);
+export const MainLayout: React.FC = ({ children }) => {
+  console.log(useLocation());
+  return (
+    <Flex
+      flex={1}
+      direction={'column'}
+      className={cx({
+        centerLayout: !(useLocation().pathname === '/')
+      })}
+    >
+      {children}
+    </Flex>
+  );
+};
