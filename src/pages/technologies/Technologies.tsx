ee5e39766a638f248c766baf7bdd25aeff04d089
Jackson <jackson.onyango@undp.org>
2023-03-23 16:09:28 +0300

    Add components and data to technologies page

--- src/pages/technologies/Technologies.tsx
+++ src/pages/technologies/Technologies.tsx
@@ -1,5 +1,92 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { InfoCard } from 'components/infoCard/InfoCard';
+import { loremIpsum } from 'react-lorem-ipsum';
+import { ProjectsCollection } from 'components/projectsCollection/ProjectsCollection';
+import { AppConst } from 'components/constants/app';
 
-export const Technologies: React.FC = () => (
-  <h3 style={{ paddingTop: '10vh' }}>Technologies Page</h3>
-);
+import { useRadarState } from '@undp_sdg_ai_lab/undp-radar';
+import { BlipType, TechItemType } from '@undp_sdg_ai_lab/undp-radar/dist/types';
+import './Technologies.scss';
+
+export const Technologies: React.FC = () => {
+  const {
+    state: { blips, radarData }
+  } = useRadarState();
+
+  const [techList, setTechList] = useState<TechItemType[]>([]);
+
+  const getTechList = (): void => {
+    setTechList(radarData.tech);
+  };
+
+  const getThreeRandomBlips = (projects: BlipType[]): BlipType[] => {
+    const result = [];
+    const length = projects.length;
+    const size = length > 3 ? 3 : length;
+    for (let i = 0; i < size; i++) {
+      const randomIndex = Math.floor(Math.random() * length);
+      result.push(projects[randomIndex]);
+    }
+
+    return result;
+  };
+
+  useEffect(() => {
+    getTechList();
+  }, []);
+
+  return (
+    <>
+      <div className='technologies'>
+        {techList.map((technology, idx) => {
+          const techProjects = blips.filter((i) =>
+            i['Technology'].includes(technology.type)
+          );
+          const techDescription = AppConst.technologyDescriptions.get(
+            technology.slug
+          ) as string[];
+          return (
+            <div
+              className='technologiesContainer'
+              key={`${idx}${technology.uuid}`}
+            >
+              <div className='topRow'>
+                <span className='technology'> {technology.type}</span>
+                {techProjects.length > 3 && (
+                  <a
+                    className='seeAll'
+                    href='#'
+                  >{`See All (${techProjects.length})`}</a>
+                )}
+              </div>
+              <div className='detailsSection'>
+                <div className='disasterDetails'>
+                  <InfoCard
+                    title={`What ${
+                      technology.type.at(-1) === 's' ? 'are' : 'is'
+                    } ${technology.type}?`}
+                    details={
+                      techDescription
+                        ? techDescription[0]
+                        : loremIpsum({
+                            p: 1,
+                            avgSentencesPerParagraph: 10,
+                            avgWordsPerSentence: 7
+                          })[0]
+                    }
+                    btnProps={{ text: 'More Info', link: '#' }}
+                  />
+                </div>
+                {
+                  <ProjectsCollection
+                    projects={getThreeRandomBlips(techProjects)}
+                  />
+                }
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
