e7a3783645eb80095d756cceff56e79e674b04ce
Jackson <jackson.onyango@undp.org>
2023-04-10 13:05:20 +0300

    Projects list

--- src/pages/projects/ProjectsList.tsx
+++ src/pages/projects/ProjectsList.tsx
@@ -1,5 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export const Projects: React.FC = () => (
-  <h3 style={{ paddingTop: '10vh' }}>Projects Page</h3>
-);
+import {
+  projectSearch,
+  mergeDisasterCycle
+} from 'components/shared/helpers/HelperUtils';
+
+import { Project } from './projectComponent/Project';
+import {
+  BaseCSVType,
+  BlipType,
+  useRadarState
+} from '@undp_sdg_ai_lab/undp-radar';
+
+import './Projects.scss';
+
+export const Projects: React.FC = () => {
+  const [query, setQuery] = useState('');
+  const [projectResults, setProjectResults] = useState<BaseCSVType[]>();
+  const [filteredProjects, setFilteredProjects] = useState<BlipType[]>();
+
+  console.log({ projectResults }, setFilteredProjects);
+
+  const {
+    state: { blips }
+  } = useRadarState();
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    const results = projectSearch(
+      event.target.value,
+      filteredProjects as BlipType[]
+    );
+    setQuery(event.target.value);
+    setProjectResults(results);
+  };
+
+  return (
+    <div className='projectsList'>
+      <div className='searchFilter'>
+        <input
+          placeholder='Search ....'
+          className='searchBar'
+          value={query}
+          onChange={handleSearch}
+        />
+      </div>
+      <div className='projectsListContainer'>
+        <div className='projectContainer'>
+          {mergeDisasterCycle(blips).map((project) => (
+            <div key={project.id}>
+              <Project project={project} />
+              <hr />
+            </div>
+          ))}
+        </div>
+
+        <div className='filters'></div>
+      </div>
+    </div>
+  );
+};
