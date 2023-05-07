69ee6b5641d165fc3071532dbc1b048dbc1b3333
Jackson <jackson.onyango@undp.org>
2023-05-07 15:14:59 +0300

    Add database url

--- src/helpers/Loader.tsx
+++ src/helpers/Loader.tsx
@@ -2,25 +2,15 @@ import { Flex, Skeleton } from '@chakra-ui/react';
 
 export const Loader: React.FC = () => (
   <>
-    <Flex pt={20} gap={10}>
-      <Skeleton height='200px' width='40%' />
-      <Skeleton height='200px' width='20%' />
-      <Skeleton height='200px' width='20%' />
-      <Skeleton height='200px' width='20%' />
-    </Flex>
-
-    <Flex pt={20} gap={10}>
-      <Skeleton height='200px' width='40%' />
-      <Skeleton height='200px' width='20%' />
-      <Skeleton height='200px' width='20%' />
-      <Skeleton height='200px' width='20%' />
-    </Flex>
-
-    <Flex pt={20} gap={10}>
-      <Skeleton height='200px' width='40%' />
-      <Skeleton height='200px' width='20%' />
-      <Skeleton height='200px' width='20%' />
-      <Skeleton height='200px' width='20%' />
-    </Flex>
+    {[...Array(3)].map((_, idx) => {
+      return (
+        <Flex pt={20} gap={10} key={idx}>
+          <Skeleton height='200px' width='40%' />
+          <Skeleton height='200px' width='20%' />
+          <Skeleton height='200px' width='20%' />
+          <Skeleton height='200px' width='20%' />
+        </Flex>
+      );
+    })}
   </>
 );
--- src/helpers/databaseClient.tsx
+++ src/helpers/databaseClient.tsx
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://sxmzetpbqzjchodypatn.supabase.co';
-const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
-// const supabaseKey =
-//   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN4bXpldHBicXpqY2hvZHlwYXRuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMwMjMzOTEsImV4cCI6MTk5ODU5OTM5MX0.9iJ44OhfPGJ7zinXM3M7rattWAb5r12oP-XpHlvILeQ';
-export const supabase = createClient(supabaseUrl, supabaseKey as string);
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL as string;
+const supabaseKey = process.env.REACT_APP_SUPABASE_KEY as string;
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
