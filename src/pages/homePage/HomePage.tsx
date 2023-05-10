33d050e77903b59f8712c0efce5ef606091d88e6
shljessie <sl994@cornell.edu>
2023-05-10 18:11:50 +0300

    issue 341: project section get 3 projects to display in homepage

--- src/pages/homePage/HomePage.tsx
+++ src/pages/homePage/HomePage.tsx
@@ -1,12 +1,51 @@
 import './HomePage.scss';
 
+import React, { useContext, useEffect, useState } from 'react';
+
 import { Image } from '@chakra-ui/react';
-// import { Link } from 'react-router-dom';
-import React from 'react';
+import { Project } from '../../pages/projects/projectComponent/Project';
+import { RadarContext } from 'navigation/context';
 import { aboutContentList } from 'pages/about/AboutContent';
 import logo from '../../assets/FTRDRR.svg';
+import { supabase } from 'helpers/databaseClient';
+
+const VERSION = process.env.REACT_APP_DISASTER_DATA_VERSION || 'version- 01';
 
 export const HomePage: React.FC = () => {
+  const [projectsToUse, setProjectsToUse] = useState<any>([]);
+  const { projectsGroup } = useContext(RadarContext);
+  const MAX_PROJECTS = 3;
+
+  useEffect(() => {
+    if (projectsGroup.length) {
+      const firstThreeProjects = projectsGroup.slice(0, MAX_PROJECTS);
+      setProjectsToUse(firstThreeProjects);
+    } else {
+      void getProjects();
+    }
+  });
+
+  const getProjects = async (): Promise<any> => {
+    const storedProjects = localStorage.getItem('projectsList');
+    if (storedProjects) {
+      const { data } = JSON.parse(storedProjects);
+      const firstThreeProjects = data.slice(0, MAX_PROJECTS);
+      setProjectsToUse(firstThreeProjects);
+    } else {
+      const { data, error } = await supabase.from('projects').select();
+      if (!error) {
+        const firstThreeProjects = data.slice(0, MAX_PROJECTS);
+        setProjectsToUse(firstThreeProjects);
+        localStorage.setItem(
+          'projectsList',
+          JSON.stringify({
+            version: VERSION,
+            data
+          })
+        );
+      }
+    }
+  };
   return (
     <div className='homePage'>
       <div className='logoSection'>
@@ -30,6 +69,16 @@ export const HomePage: React.FC = () => {
             )}
           </div>
         </div>
+      </div>
+      <div className='projectSection'>
+        {projectsToUse
+          .map((project: any) => (
+            <div key={project.id}>
+              <Project project={project} />
+              <hr />
+            </div>
+          ))
+          .slice(0, MAX_PROJECTS)}
       </div>
     </div>
   );
