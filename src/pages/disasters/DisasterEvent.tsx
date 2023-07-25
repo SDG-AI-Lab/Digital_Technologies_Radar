2976f557b490c3413941a4b386d42e0c3fe02187
Jackson <jackson.onyango@undp.org>
2023-07-25 15:30:19 +0300

    Dummy title

--- src/pages/disasters/DisasterEvent.tsx
+++ src/pages/disasters/DisasterEvent.tsx
@@ -14,11 +14,10 @@ export const DisasterEvent: React.FC = () => {
       'data',
       'contacts'
     ],
-    description:
-      'An application programming interface (API) is a connection between computers or between computer programs. It is a type of software interface, offering a service to other pieces of software. A document or standard that describes how to build or use such a connection or interface is called an API specification. A computer system that meets this standard is said to implement or expose an API. The term API may refer either to the specification or to the implementation.',
+    description: DUMMY_TEXT,
     img_url:
-      'https://images.unsplash.com/photo-1667372393086-9d4001d51cf1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80',
-    name: 'Application Programming Interface',
+      'https://images.unsplash.com/photo-1677233860259-ce1a8e0f8498?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    name: '2023 Turkey/Syria Earthquake',
     slug: 'application-programming-interface',
     source: 'https://www.google.com',
     overview: DUMMY_TEXT,
