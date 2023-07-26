3c8ed9350bd9f4bd561862b29865fd7e7f4725b2
Jackson <jackson.onyango@undp.org>
2023-07-26 14:41:44 +0300

    Format component

--- src/pages/homePage/components/RecentDisasters.tsx
+++ src/pages/homePage/components/RecentDisasters.tsx
@@ -1,3 +1,4 @@
+import { Badge } from '@chakra-ui/react';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
@@ -15,13 +16,28 @@ interface Props {
 
 export const RecentDisasters: React.FC<Props> = ({ recentDisasters }) => (
   <div className='recentDisastersCollection'>
-    {recentDisasters.map((disaster) => (
-      <>
-        <Link to={'/disaster_events/id'} key={disaster.id}>
-          <h4>{disaster.title}</h4>
-        </Link>
-        <p>{disaster.summary}</p>
-      </>
-    ))}
+    <div className='disastersList'>
+      {recentDisasters.map((disaster) => (
+        <div className='disasterEvent' key={disaster.id}>
+          <Link to={'/disaster_events/id'} key={disaster.id}>
+            <p className='disasterEvent-title'>{disaster.title}</p>
+          </Link>
+          <p className='disasterEvent-summary'>{disaster.summary}</p>
+        </div>
+      ))}
+    </div>
+    <div className='urgentBadge'>
+      <Badge
+        px={3}
+        py={1}
+        borderRadius='lg'
+        bg='#C1391D'
+        color='white'
+        w='fit-content'
+        h='fit-content'
+      >
+        URGENT
+      </Badge>
+    </div>
   </div>
 );
