6b65b48682d791005d55f0e38e49c34da696f300
Jake <jakepyne@gmail.com>
2023-09-11 22:18:50 +0200

    Restructure e2e tests

--- cypress/e2e/create-project.cy.ts
+++ cypress/e2e/create-project.cy.ts
@@ -1,13 +1,3 @@
-// Command for selecting a value in our custom dropowns
-Cypress.Commands.add('selectFirstDropdownOption', (selector) => {
-  cy.get(selector + ' .rmsc > .dropdown-container').click();
-
-  cy.get(
-    selector +
-      ' .rmsc > .dropdown-container > .dropdown-content input[placeholder="Search"]'
-  ).type('{downArrow}{enter}{esc}');
-});
-
 describe('create project', () => {
   // Set logged in state
   beforeEach(() => {
